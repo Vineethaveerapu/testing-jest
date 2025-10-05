18a49c5d25524230373f5bb7d3691baee278ecea
Vineetha Veerapu <vineethaveerapu4@gmail.com>
2025-10-06 01:55:31 +0200

    add contact page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,3 +1,5 @@
+import TextCard from '@/components/TextCard';
+
 export const metadata = {
   title: 'Contact Us',
   description: 'Contact Us page',
@@ -5,8 +7,37 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <div>
+    <div className="space-y-4">
       <h1>Contact Us</h1>
+
+      <p>
+        We are here to help you. Please contact us for any questions or
+        inquiries.
+      </p>
+
+      <div className="flex gap-4 flex-wrap">
+        <TextCard title="Address">
+          <p>
+            123 Garden Street
+            <br />
+            Green Valley, Stockholm, Sweden
+          </p>
+        </TextCard>
+        <TextCard title="Email">
+          <p>
+            +46 70 123 45 67
+            <br />
+            Mon-Fri: 9:00 AM - 6:00 PM
+          </p>
+        </TextCard>
+        <TextCard title="Phone">
+          <p>
+            +46 70 123 45 67
+            <br />
+            Mon-Fri: 9:00 AM - 6:00 PM
+          </p>
+        </TextCard>
+      </div>
     </div>
   );
 }
--- src/components/TextCard.tsx
+++ src/components/TextCard.tsx
@@ -1,15 +1,15 @@
 interface TextCardProps {
   className?: string;
   title: string;
-  description: string;
   children: React.ReactNode;
 }
 
 const TextCard = ({ className = '', title, children }: TextCardProps) => {
   return (
-    <div className={`${className} border-2 border-gray-300 rounded-md p-4`}>
-      <h3 className="text-lg font-bold">{title}</h3>
-      <div>{children}</div>
+    <div
+      className={`${className} text-center p-8 bg-gray-50 rounded-lg transition-transform duration-300 ease-in-out`}>
+      <h2 className="text-blue-600 mb-4 text-2xl font-bold">{title}</h2>
+      <div className="text-gray-600 my-2 text-lg">{children}</div>
     </div>
   );
 };
