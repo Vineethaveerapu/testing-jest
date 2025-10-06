f44dcb1102ee4403399a1fdcd8723ab30d132876
Vineetha Veerapu <vineethaveerapu4@gmail.com>
2025-10-06 22:42:10 +0200

    create data folder

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,6 @@
-export const pageLinks: { name: string; href: string }[] = [
+import type { PageLink, Service } from '@/types';
+
+export const pageLinks: PageLink[] = [
   {
     name: 'Home',
     href: '/',
@@ -6,5 +8,26 @@ export const pageLinks: { name: string; href: string }[] = [
   {
     name: 'Contact Us',
     href: '/contact-us',
+  },
+];
+
+export const ourServices: Service[] = [
+  {
+    id: 1,
+    title: 'Weeding',
+    description:
+      'Weeding is the process of removing unwanted plants from a garden or lawn. It is a necessary part of maintaining a healthy garden and lawn.',
+  },
+  {
+    id: 2,
+    title: 'Watering',
+    description:
+      'Watering is the process of adding water to a garden or lawn. It is a necessary part of maintaining a healthy garden and lawn.',
+  },
+  {
+    id: 3,
+    title: 'Pruning and Trimming',
+    description:
+      'Pruning and trimming is the process of cutting back plants to maintain a healthy garden and lawn.',
   },
 ];
