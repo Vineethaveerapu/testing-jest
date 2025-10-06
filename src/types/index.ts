fb0bd79bd7df11e6dc07f39867a9029d3cc39123
Vineetha Veerapu <vineethaveerapu4@gmail.com>
2025-10-06 22:42:02 +0200

    move all types to types folder

---
+++ src/types/index.ts
@@ -0,0 +1,41 @@
+export interface Service {
+  id: number;
+  title: string;
+  description: string;
+}
+
+export interface PageLink {
+  name: string;
+  href: string;
+}
+
+export interface BaseComponentProps {
+  className?: string;
+}
+
+export interface ServiceItemProps extends BaseComponentProps {
+  id: number;
+  title: string;
+  description: string;
+  isSelectedList: boolean;
+  handleClickService: (service: Service) => void;
+}
+
+export interface ServiceListProps extends BaseComponentProps {
+  items: Service[];
+  addSelectedService?: (service: Service) => void;
+  removeSelectedService?: (service: Service) => void;
+  isSelectedList?: boolean;
+}
+
+export interface MediaCardProps extends BaseComponentProps {
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+}
+
+export interface TextCardProps extends BaseComponentProps {
+  title: string;
+  children: React.ReactNode;
+}
